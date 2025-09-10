d8bd75178719dbb72836cd49cdc1a97d4104b47a
tuginecologo <gustavorojas616@gmail.com>
2025-09-09 22:25:24 -0500

    modificación 1

--- src/config/database.js
+++ src/config/database.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST || 'localhost',
   user: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD || '',
-  database: process.env.MYSQLDATABASE || 'ASP',
+  database: process.env.MYSQLDATABASE || 'railway',
   port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
